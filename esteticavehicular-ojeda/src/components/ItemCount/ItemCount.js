f954663ebf47896b6ac445a545aa8bc0aa020903
victoroj21 <ojedavh@gmail.com>
2021-09-22 21:23:50 -0300

    correccion entrega itemCount
    agrego el call back

--- esteticavehicular-ojeda/src/components/ItemCount/ItemCount.js
+++ esteticavehicular-ojeda/src/components/ItemCount/ItemCount.js
@@ -10,22 +10,30 @@ import DoNotDisturbOnTwoToneIcon from '@mui/icons-material/DoNotDisturbOnTwoTone
 
 const ItemCount = (props) => {
     const [stock, setStock] = useState(props.initial);
+    const [disabled, disabledButton] = useState(true);
 
-    const onAdd = () => {
-        if (stock < props.stock)
+    const agregar = () => {
+        if (stock < props.stock) {
             setStock(stock + 1);
+            disabledButton(false);
+        }
     }
 
-    const onSubtract = () => {
-        if (stock > 0)
+    const quitar = () => {
+        if (stock > 1){
             setStock(stock - 1);
+        }
+        else if (stock == 1){
+            setStock(stock - 1);
+            disabledButton(true);
+        }
 
     }
 
     return (
         <Grid container spacing={0.5} justifyContent="center">
             <Grid item xs={1}>{/* boton - */}
-                <IconButton onClick={onSubtract} color="primary" aria-label="resta stock" >
+                <IconButton onClick={quitar} color="primary" aria-label="resta stock" >
                     <DoNotDisturbOnTwoToneIcon />
                 </IconButton>
             </Grid>
@@ -33,12 +41,12 @@ const ItemCount = (props) => {
                 <TextField value={stock} size="small" label="Cantidad" variant="outlined" />
             </Grid>
             <Grid item xs={1}> {/* boton + */}
-                <IconButton onClick={onAdd} color="primary" aria-label="suma stock" >
+                <IconButton onClick={agregar} color="primary" aria-label="suma stock" >
                     <ControlPointTwoToneIcon />
                 </IconButton>
             </Grid>
             <Grid item xs={12}>
-                <Button variant="contained">Agregar al carrito</Button>
+                <Button disabled={disabled} onClick={props.onAdd(stock)} variant="contained">Agregar al carrito</Button>
             </Grid>
         </Grid>
     );
--- esteticavehicular-ojeda/src/components/ItemListContainer/ItemListContainer.js
+++ esteticavehicular-ojeda/src/components/ItemListContainer/ItemListContainer.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ItemCount from '../ItemCount/ItemCount';
 
 const ItemListContainer = (producto) => {
-  
+
+  const onAdd = (unidades) => {
+    console.log(`Se agregaron las ${unidades} unidades correctamente al carrito`)
+  }
+
   return (
     <div className="ItemListContainer">
       <p>Producto: <b>{producto.nombre}</b> <br />
         Precio: <b>${producto.precio}</b><br />
         Stock disponible: <b>{producto.stock}</b></p>
-       <ItemCount stock={producto.stock} initial={0}/> 
+      <ItemCount stock={producto.stock} initial={0} onAdd={onAdd}/>
     </div>
   );
 }
